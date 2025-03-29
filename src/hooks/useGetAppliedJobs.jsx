b3ae10307246819d9e488c8521bf3ddb818954e5
Radhika Tayal <116450121+Radhikatayal80@users.noreply.github.com>
2025-03-29 16:48:18 +0530

    Update useGetAppliedJobs.jsx

--- src/hooks/useGetAppliedJobs.jsx
+++ src/hooks/useGetAppliedJobs.jsx
@@ -1,25 +1,30 @@
 import { setAllAppliedJobs } from "@/redux/jobSlice";
 import { APPLICATION_API_END_POINT } from "@/utils/constant";
-import axios from "axios"
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import axios from "axios";
+import { useEffect, useCallback } from "react";
+import { useDispatch } from "react-redux";
 
 const useGetAppliedJobs = () => {
     const dispatch = useDispatch();
 
-    useEffect(()=>{
-        const fetchAppliedJobs = async () => {
-            try {
-                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
-                console.log(res.data);
-                if(res.data.success){
-                    dispatch(setAllAppliedJobs(res.data.application));
-                }
-            } catch (error) {
-                console.log(error);
+    const fetchAppliedJobs = useCallback(async () => {
+        try {
+            console.log("Fetching applied jobs...");
+            const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, { withCredentials: true });
+
+            if (res.data.success) {
+                dispatch(setAllAppliedJobs(res.data.application));
+            } else {
+                console.warn("API request was successful but returned false status.");
             }
+        } catch (error) {
+            console.error("Error fetching applied jobs:", error.response?.data || error.message);
         }
+    }, [dispatch]);
+
+    useEffect(() => {
         fetchAppliedJobs();
-    },[])
+    }, [fetchAppliedJobs]);
 };
+
 export default useGetAppliedJobs;
